a9257dee13ac36d8f92e7666fa0fca239fcf02e1
adiamandouros <adiamandouros@gmail.com>
2025-10-01 13:28:00 +0300

    changed db config

--- model/config.mjs
+++ model/config.mjs
@@ -10,26 +10,13 @@ const sequelize = new Sequelize(
     // },
     // storage: './data/barbutia.sqlite'
     // }
-    process.env.DATABASE_URL,
-    {
-        dialect:'mysql',
-        database: 'barbutia',
-        user: 'baruser',
-        password: 'sasgamanetabarbutia',
-        host:'localhost',
-        port:'3306',
-        logging: false,
-        define:{
-                    timestamps: false,
-                    freezeTableName: true
-                },
-        dialectOptions: {
-            ssl:{
-                require:true,
-                rejectUnauthorized: false
-            }
-        }
+    process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    logging: false,
+    define: {
+        timestamps: false,
+        freezeTableName: true
     }
-    );
+});
 
 export default sequelize
